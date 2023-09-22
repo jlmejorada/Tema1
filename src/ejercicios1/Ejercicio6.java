9b628c5acb3a30b8743f3eb00f04482a418617e7
jlmejorada <jlmejorada@106-18.info2>
2023-09-22 14:37:16 +0200

    Ahi va

--- src/ejercicios1/Ejercicio6.java
+++ src/ejercicios1/Ejercicio6.java
@@ -1,10 +1,29 @@
 package ejercicios1;
+import java.util.Scanner;
 
 public class Ejercicio6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Creamos las variables necesarias
+		int num1, num2, suma, resta, mul, div;
+		Scanner sc = new Scanner(System.in);
+		
+		//Pedimos dos números
+		System.out.println("Dame un número");
+		num1 = sc.nextInt();
+		System.out.println("Dame otro número");
+		num2 = sc.nextInt();
+		
+		//Hacemos los calculos y los mostramos por pantalla
+		suma= num1 + num2;
+		resta= num1 - num2;
+		mul= num1 * num2;
+		div= num1 / num2;
+		System.out.println("La suma de los números es: " + suma);
+		System.out.println("La resta de los números es: " + resta);
+		System.out.println("La multiplicación de los números es: " + mul);
+		System.out.println("La división de los números es: " + div);
+		sc.close();
 	}
 
 }
