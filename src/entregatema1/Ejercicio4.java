c21ebdac777a15b7d950a919cf060ebe88f867dc
jlmejorada <jlmejorada@106-18.info2>
2023-09-27 10:11:26 +0200

    Ejercicio 4

---
+++ src/entregatema1/Ejercicio4.java
@@ -0,0 +1,31 @@
+package entregatema1;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		// Creamos las variables para la base, altura y area del triangulo
+		float base, altura, area;
+		
+		//Creamos una variable scanner
+		Scanner sc = new Scanner(System.in);
+
+		//Pedimos la base y la altura del triangulo
+		System.out.println("Dame la base del triángulo");
+		base = sc.nextFloat();
+		
+		System.out.println("Dame la altura del triángulo");
+		altura = sc.nextFloat();
+		
+		//Calculamos el área del triángulo
+		area = (base*altura)/2;
+		
+		//Mostramos el area por pantalla
+		System.out.println("El área de tu triangulo de es " + area);
+		
+		//Cerramos el scanner
+		sc.close();
+	}
+
+}
