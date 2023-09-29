55d38218a8ee7e90fa592a88abe9a402bf08e702
jlmejorada <jlmejorada@106-18.info2>
2023-09-29 13:45:10 +0200

    actualización 2

--- src/entregatema1/Ejercicio2.java
+++ src/entregatema1/Ejercicio2.java
@@ -8,6 +8,10 @@ public class Ejercicio2 {
 		// Creamos una variable que recoja un número entero y el resto de modulo
 		int num, resto;
 		
+		//Creamos la constante para el número 7 y otra para el 0
+		final int num7=7;
+		final int num0=0;
+		
 		//Creamos una variable scanner
 		Scanner sc = new Scanner(System.in);
 		
@@ -16,8 +20,8 @@ public class Ejercicio2 {
 		num = sc.nextInt();
 		
 		//Calculamos a traves de su modulo, cuando le queda oara ser multiplo de 7
-		resto = (num%7);
-		resto = (resto == 0)? resto : 7 - resto;
+		resto = (num%num7);
+		resto = (resto == num0)? resto : num7 - resto;
 		
 		//Mostramos por pantalla, cuanto le queda al número para ser multiplo de 7
 		System.out.println("A tu número le falta sumarle " + resto + " para ser multiplo de 7");
